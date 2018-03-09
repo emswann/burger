7baec47d4b9820e370978724e653fc9c53089b6c
Elaina Swann <elaina@odecisions.com>
2018-03-09 14:39:49 -0500

    Adds JAWSDB connection for Heroku.

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,19 @@
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
-  port: 3308,
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'burgers_db'
-});
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+  createConnection({
+    port: 3308,
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'burgers_db'
+  });
+};
 
 connection.connect(err =>
   err ? console.error('Error connecting: ' + err.stack)
